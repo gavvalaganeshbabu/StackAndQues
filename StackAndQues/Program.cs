2b8af6a905ecbbe55335b901d1f16ca2bc7a027d
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 10:13:37 +0530

    Add project files.

---
+++ StackAndQues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Ques Application");
+        }
+    }
+}
